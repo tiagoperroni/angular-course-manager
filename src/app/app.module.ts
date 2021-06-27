b3adca7875d88d398e493d80987bb6b8af4ea36a
tiagoperroni <tiagoperroni86@gmail.com>
2021-06-27 11:33:32 -0300

    chaging dates with pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { starComponent } from './star/star.component';
+import { ReplacePipe } from './pipe/replace.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    starComponent
+    starComponent,
+    ReplacePipe
   ],
   imports: [
     BrowserModule,
