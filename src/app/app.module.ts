79ed7909ae243a5860920f663eafeeaf488c71df
tiagoperroni <tiagoperroni86@gmail.com>
2021-06-27 15:00:50 -0300

    protect paths with guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,34 @@ import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { starComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { Error404Com } from './courses/error-404/error-404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     starComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent,
+    Error404Com
+  
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: '**', component: Error404Com
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
