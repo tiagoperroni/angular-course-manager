0aa1b134b83659c9539a4a231cececfc0572086e
tiagoperroni <tiagoperroni86@gmail.com>
2021-06-27 15:45:48 -0300

    paths to acess components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { Error404Com } from './courses/error-404/error-404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { Error404Com } from './courses/error-404/error-404.component';
     starComponent,
     ReplacePipe,
     NavBarComponent,
-    Error404Com
+    Error404Com,
+    CourseInfoComponent
   
   ],
   imports: [
@@ -25,10 +27,13 @@ import { Error404Com } from './courses/error-404/error-404.component';
     FormsModule,
     RouterModule.forRoot([
       {
-        path: '', redirectTo: 'courses', pathMatch: 'full'
+        path: 'courses', component: CourseListComponent
       },
       {
-        path: 'courses', component: CourseListComponent
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
       },
       {
         path: '**', component: Error404Com
